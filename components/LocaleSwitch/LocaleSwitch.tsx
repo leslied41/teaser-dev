1dc3fdb111fafb11f623f95478ecb40029d3f4ab
leslieduan <zduan@utas.edu.au>
2022-09-09 15:46:22 +1000

    change default locale to cn

--- components/LocaleSwitch/LocaleSwitch.tsx
+++ components/LocaleSwitch/LocaleSwitch.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo } from "react";
+import React, { memo } from "react";
 import { useRouter } from "next/router";
 import cn from "clsx";
 
--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
-    locales: ["en", "cn"],
-    defaultLocale: "en",
+    locales: ["cn", "en"],
+    defaultLocale: "cn",
   },
 };
 
