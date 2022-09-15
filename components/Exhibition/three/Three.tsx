13cd77a75d882d5edcaa6e472121eaa3f596c65c
leslieduan <zduan@utas.edu.au>
2022-09-15 19:38:06 +1000

    fix bug

--- components/Exhibition/three/Three.tsx
+++ components/Exhibition/three/Three.tsx
@@ -9,37 +9,32 @@ interface Props {
   order: number;
 }
 const Three: FC<Props> = ({ order }) => {
-  const textures_one = useMemo(
-    () =>
-      useLoader(TextureLoader, [
-        "/models/test/front.jpeg",
-        "/models/test/back.jpeg",
-        "/models/test/top.jpeg",
-        "/models/test/bottom.jpeg",
-        "/models/test/left.jpeg",
-        "/models/test/right.jpeg",
-      ]),
-    []
-  );
-  const textures_two = useMemo(
-    () =>
-      useLoader(TextureLoader, [
-        "/models/test_2/front.jpeg",
-        "/models/test_2/back.jpeg",
-        "/models/test_2/top.jpeg",
-        "/models/test_2/bottom.jpeg",
-        "/models/test_2/left.jpeg",
-        "/models/test_2/right.jpeg",
-      ]),
-    []
-  );
+  const textures_one = () =>
+    useLoader(TextureLoader, [
+      "/models/test/front.jpeg",
+      "/models/test/back.jpeg",
+      "/models/test/top.jpeg",
+      "/models/test/bottom.jpeg",
+      "/models/test/left.jpeg",
+      "/models/test/right.jpeg",
+    ]);
+
+  const textures_two = () =>
+    useLoader(TextureLoader, [
+      "/models/test_2/front.jpeg",
+      "/models/test_2/back.jpeg",
+      "/models/test_2/top.jpeg",
+      "/models/test_2/bottom.jpeg",
+      "/models/test_2/left.jpeg",
+      "/models/test_2/right.jpeg",
+    ]);
 
   const textures = useMemo(() => {
     let texture;
     if (order === 0) texture = textures_one;
     if (order === 1) texture = textures_two;
     return texture;
-  }, [order]);
+  }, [order, textures_one, textures_two]);
 
   return (
     <>
